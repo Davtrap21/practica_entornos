ec85f6f1e30e5cd60f26c5e33c8ff3a0924921c3
Davtrap21 <74045780+Davtrap21@users.noreply.github.com>
2024-05-10 21:48:36 +0000

    Update PVZAllStarZombieEntity.java

--- PVZAllStarZombieEntity.java
+++ PVZAllStarZombieEntity.java
@@ -51,7 +51,7 @@ public class PVZAllStarZombieEntity extends ZombieEntity implements IAnimatable 
 	protected PlayState predicate(final AnimationEvent event) {
 		if (event.isMoving()) {
 			event.getController().setAnimation(ModEntityGeoAnimations.PVZ_ALLSTAR_RUNNING_ANIM);
-			return PlayState.CONTINUE; // WTF are you doing
+			return PlayState.CONTINUE; // WTF are you doing leave this in continue
 		}
 
 		event.getController().setAnimation(ModEntityGeoAnimations.PVZ_ZOMBIE_IDLE_ANIM);
